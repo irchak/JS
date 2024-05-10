e851329f66df23672615bf324011f43510cffe27
Lidia <irchaklidia@gmail.com>
2024-05-10 08:11:46 +0300

    Add variables

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+let firstVariable = 2;
+let lastVariable = 3;
+let result = firstVariable * lastVariable;
+console.log(result);
+
+let c = 4;
+let d = 2;
+let division = c / d;
+console.log(division);
+
+let e = 5;
+let f = 6;
+let addition = e + f;
+console.log(addition);
+
+let number = 11;
+let woman = true;
+let nameUser = "java script";
+let number2 = "100";
+console.log(number, woman, nameUser, number2);
+
+let num = 1;
+num += 11;
+alert(num);
+
+num -= 11;
+alert(num);
+
+num *= 11;
+alert(num);
+
+num /= 11;
+alert(num);
+
+num++;
+alert(num);
+
+num--;
+alert(num);
